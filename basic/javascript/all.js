d410d4cacdd7be7bafeeec8403d0c68b42127014
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2022-02-18 16:05:53 +0800

    first

---
+++ basic/javascript/all.js
@@ -0,0 +1,10 @@
+const myName = 'Ray'
+const url = 'https://israynotarray.com/'
+
+function callName (name) {
+  if (name === 'Ray') {
+    window.alert('My Name is ' + name + url)
+  }
+}
+
+callName(myName)
